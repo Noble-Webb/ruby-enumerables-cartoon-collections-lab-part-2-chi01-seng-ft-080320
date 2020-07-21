124ea4b2e45044fe58d54a960c91576b4985ff59
Justin Webb <webb.n.justin@gmail.com>
2020-07-21 02:17:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do  |plan| 
-       plan == valid_calls.map
+       plan == valid_calls.include?
      
   
   end  
